c7d51fb2bdccafa24d5efebad16084d74b9c25c1
Henry <henry32144@hotmail.com>
2017-06-16 23:25:52 +0800

    Use Knockout.js and change some place which used JQuery before

--- js/app.js
+++ js/app.js
@@ -1,4 +1,43 @@
-$("#menu-toggle").click(function(e) {
-          $("#wrapper").toggleClass("toggled");
-          console.log("123");
-      });
+
+var initialCats = [
+	{
+		name : 'Tabby',
+	},
+	{
+		name : 'Tiger',
+	},
+	{
+		name : 'Black',
+	},
+	{
+		name : 'White',
+	},
+	{
+		name : 'Fan',
+	},
+];
+
+var Cat = function(data) {
+	this.name = ko.observable(data.name);
+}
+
+var ViewModel = function() {
+	var self = this;
+
+	isToggled = ko.observable(false);
+
+	this.toggleSideBar = function() {
+		isToggled(!isToggled());
+	}
+
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push (new Cat(catItem));
+	});
+
+	this.currentCat = ko.observable(this.catList()[0]);
+
+}
+
+ko.applyBindings(new ViewModel());
