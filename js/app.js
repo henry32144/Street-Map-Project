5844687bba436f90ec8b9fdb030387fce4c32946
Henry <henry32144@hotmail.com>
2017-06-18 21:19:57 +0800

    Filter now can toggle markers Visible and add some comments

--- js/app.js
+++ js/app.js
@@ -4,15 +4,10 @@ var saveStoreList = function(place) {
 	stores(place);
 }
 
-var createStoreList = function(place) {
-	console.log(place.name);
+var createStoreList = function(place , index) {
 	this.name = ko.observable(place.name);
 	this.isVisible = ko.observable(true);
-}
-
-var Cat = function(data) {
-	this.name = ko.observable(data.name);
-	this.isVisible = ko.observable(true);
+	this.index = index;
 }
 
 var ViewModel = function() {
@@ -30,13 +25,11 @@ var ViewModel = function() {
 	this.storeList = ko.observableArray([]);
 
 	this.refreshStoreList = ko.computed(function() {
-		stores().forEach(function(storeItem) {
-		self.storeList.push (new createStoreList(storeItem));
+		stores().forEach(function(storeItem , index) {
+		self.storeList.push(new createStoreList(storeItem , index));
 		});
 	},this);
 	
-
-
 
 	this.autoCompelete = ko.computed(function() {
 		this.reg = new RegExp(this.searchValue(),"i");
@@ -44,10 +37,16 @@ var ViewModel = function() {
 			if(!this.reg.test(this.storeList()[s].name()))
 			{
 				this.storeList()[s].isVisible(false);
+				if(markersReady) {
+					hideMarker(this.storeList()[s].index);
+				}
 			}
 			else
 			{
 				this.storeList()[s].isVisible(true);
+				if(markersReady) {
+					showMarker(this.storeList()[s].index);
+				}
 			}
 		}
 
--- js/map.js
+++ js/map.js
@@ -1,22 +1,34 @@
 var map;
+//create blank markers array
+var markers = [];
+//create defaultPLace to display when the geolocation is not work
 var defaultPlace;
+//create blank infoWindow
 var infowindow;
+//create google place api service variable
 var service;
+//check if markers is ready to use
+var markersReady = false;
 
+//this function is to generate map , it will called when the api is loaded.
 function initMap() {
+
+    // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 25.050302, lng: 121.531105},
-        zoom: 13,
+        zoom: 14,
         fullscreenControl: false,
         mapTypeControl: false
       });
 
+      //initialize defalutPLace the location is in Taipei
       defaultPlace = new google.maps.LatLng({lat: 25.050302, lng: 121.531105});
 
       service = new google.maps.places.PlacesService(map);
 
       infowindow = new google.maps.InfoWindow();
 
+      //Try to get user's current location
       // Try HTML5 geolocation.
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -33,20 +45,9 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false);
       }
-
-
-
 }
 
-function getSearch(pos) {
-    service.nearbySearch({
-    location: pos,
-    radius: 1000,
-    keyword: '壽司',
-    type: ['restaurant']
-  }, callback);
-}
-
+//this function handle errors about user's location 
 function handleLocationError(browserHasGeolocation) {
     alert(browserHasGeolocation ?
                         'Error: The Geolocation service failed. Display default locations' :
@@ -55,9 +56,24 @@ function handleLocationError(browserHasGeolocation) {
     getSearch(defaultPlace);
 }
 
+//this function try to get sushi restaurant around
+//the location , used google place api
+function getSearch(pos) {
+    service.nearbySearch({
+    location: pos,
+    radius: 1000,
+    keyword: "(壽司) OR (寿司)",
+    type: ['restaurant']
+  }, callback);
+}
+
+
+//this function generate marker when getSearch is done and
+//send results to saveStoreList for knockout model 
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
         console.log(results);
+        //save results for model
         saveStoreList(results);
         for (var i = 0; i < results.length; i++) {
           createMarker(results[i]);
@@ -66,15 +82,47 @@ function callback(results, status) {
     else alert('Maybe there is no Sushi Around you!');
 }
 
+//this function create marker and fill in information in it
 function createMarker(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
         map: map,
+        title: place.name,
+        animation: google.maps.Animation.DROP,
         position: place.geometry.location
     });
+
+    markers.push(marker);
+
+    if(markers.length >= 19) {
+      markersReady = true;
+    }
 
     google.maps.event.addListener(marker, 'click', function() {
       infowindow.setContent(place.name);
       infowindow.open(map, this);
+      toggleBounce(this);
     });
+}
+
+//this function switch marker's bounce event
+//it will toggle off every other marker's animation
+//make sure there is only one marker is bouncing.
+function toggleBounce(marker) {
+  for(var i = 0;i < markers.length;i ++){
+    markers[i].setAnimation(null);
+  }
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function showMarker(index) {
+  markers[index].setMap(map);
+}
+
+function hideMarker(index) {
+  markers[index].setMap(null);
 }
